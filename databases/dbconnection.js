3ef298fa18e918f42cda36db1d60c1b67f453cdc
somiasaad <111437823+somiasaad@users.noreply.github.com>
2024-04-03 19:41:51 +0200

    Update dbconnection.js

--- databases/dbconnection.js
+++ databases/dbconnection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 export const dbconnection = () => {
-    mongoose.connect("mongodb+srv://graduated:graduated12@cluster0.xnavg3n.mongodb.net/graduatedproject", { useNewUrlParser: true })
+    mongoose.connect("mongodb+srv://somiasaad012:AT6qx36UmejSCFmc@cluster0.wdsvst1.mongodb.net/", { useNewUrlParser: true })
         .then(() => {
             console.log("database connected .");
         }).catch((err) => {
